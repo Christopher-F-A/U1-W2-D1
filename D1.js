6e6643922da41435b0146a6fcb93ec77a8781220
Christopher F A <christopher.armani095@gmail.com>
2025-12-01 15:31:07 +0100

    S2/L1

--- D1.js
+++ D1.js
@@ -11,38 +11,73 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+//I principali datatype in JavaScript sono:
+//
+// - string
+// È come una frase dentro virgolette. Esempio: "Ciao".
+// Come un'etichetta con delle parole scritte sopra.
+//
+// - number
+// Sono tutti i numeri: 5, 3.14, -10…
+// Come i mattoncini LEGO con un numero stampato.
+//
+// - boolean
+// Può essere SOLO true o false.
+// Come un interruttore: acceso (true) o spento (false).
+//
+// - null
+// Significa "non c’è niente", è volutamente vuoto.
+// Come una scatola che sai essere vuota.
+//
+// - undefined nan
+// Significa che qualcosa non è stato ancora definito.
+// Come una scatola senza etichetta: non sai cosa contiene.
+//
+// - object
+// È una scatola che contiene più cose insieme.
+// Esempio: { nome: "Chris", età: 30 }.
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Christopher";
+console.log(myName);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sum = 12 + 20;
+console.log(sum);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+console.log(x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+myName = "Filippi Armani";
+console.log(myName);
+
+const umano = "terrestre";
+console.log(umano);
+
+// umano = "alieno";
+// console.log(umano); // X Uncaught TypeError: Assignment to constant variable. at D1.js:71:7
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sottrazione = 4 - x;
+console.log(sottrazione);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +85,8 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john";
+let name2 = "John";
+
+console.log(name1 === name2);
+console.log(name1.toLowerCase === name2.toLowerCase);
